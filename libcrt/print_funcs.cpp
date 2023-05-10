1c1b1e0104d9a282eded440c7f1da513a59521c6
h0li3@hotmail.com <h0li3@hotmail.com>
2023-05-10 11:52:55 +0800

    去掉丑陋的GetProcAddress，完善对xxxprintf_s的支持

--- libcrt/print_funcs.cpp
+++ libcrt/print_funcs.cpp
@@ -1,26 +1,13 @@
 #pragma warning(disable : 28252)
 
+// Disable stdio inline temporarily.
+#define _NO_CRT_STDIO_INLINE
 #include <stdio.h>
-
-extern "C" void* __stdcall GetModuleHandleW(const wchar_t*);
-extern "C" void* __stdcall GetProcAddress(void*, const char*);
-
-extern "C" static decltype(_vsnprintf) * fn__vsnprintf;
-extern "C" static decltype(vfprintf) * fn_vfprintf;
-
-extern "C" int __ncrt_init_printf()
-{
-    auto msvcrt = GetModuleHandleW(L"msvcrt.dll");
-    if (!msvcrt)
-        return 255;
-    fn__vsnprintf = reinterpret_cast<decltype(_vsnprintf)*>(GetProcAddress(msvcrt, "_vsnprintf"));
-    fn_vfprintf = reinterpret_cast<decltype(vfprintf)*>(GetProcAddress(msvcrt, "vfprintf"));
-    return ((fn__vsnprintf && fn_vfprintf) ? 0 : 255);
-}
+#undef _NO_CRT_STDIO_INLINE
+#include <stdlib.h>
 
 extern "C"
 {
-
     _Success_(return >= 0)
         _ACRTIMP int __cdecl __stdio_common_vsprintf_s(
             _In_                                    unsigned __int64 _Options,
@@ -31,12 +18,20 @@ extern "C"
             va_list          _ArgList
         )
     {
-        if (_Buffer && _BufferCount > 0)
-        {
-            _Buffer[0] = 0;
-            return fn__vsnprintf(_Buffer, _BufferCount, _Format, _ArgList);
+        int len = -1;
+
+        if (_Buffer && _BufferCount != 0) {
+            len = _vsnprintf(_Buffer, _BufferCount, _Format, _ArgList);
+            if (len > _BufferCount - 1) {
+                _Buffer[0] = 0;
+                len = -1;
+            }
         }
-        return 0;
+
+#ifdef _LIBCRT_ABORT_WHEN_FAILURE
+        abort();
+#endif
+        return len;
     }
 
     _Success_(return >= 0)
@@ -50,7 +45,20 @@ extern "C"
             va_list          _ArgList
         )
     {
-        return fn__vsnprintf(_Buffer, _BufferCount, _Format, _ArgList);
+        int len = -1;
+
+        if (_Buffer && _BufferCount != 0) {
+            len = _vsnprintf(_Buffer, _BufferCount, _Format, _ArgList);
+            if (len > _BufferCount - 1) {
+                _Buffer[0] = 0;
+                len = -1;
+            }
+        }
+
+#ifdef _LIBCRT_ABORT_WHEN_FAILURE
+        abort();
+#endif
+        return len;
     }
 
     _Success_(return >= 0)
@@ -63,8 +71,6 @@ extern "C"
             va_list          _ArgList
         )
     {
-        if (_Buffer == 0)
-            return 255;
         return __stdio_common_vsprintf_s(_Options, _Buffer, _BufferCount, _Format, _Locale, _ArgList);
     }
 
@@ -76,6 +82,6 @@ extern "C"
         va_list          _ArgList
     )
     {
-        return fn_vfprintf(_Stream, _Format, _ArgList);
+        return vfprintf(_Stream, _Format, _ArgList);
     }
 }
